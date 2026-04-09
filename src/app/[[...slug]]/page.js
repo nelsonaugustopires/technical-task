e747355a6ae93817e6b2a172f5d891fe3ee3014e
Nelson Augusto Pires <nelsonaugustopires@gmail.com>
2026-04-09 18:17:00 -0300

    Internationalization

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -2,17 +2,39 @@ import { StoryblokStory } from '@storyblok/react/rsc';
 import { getStoryblokApi } from '@/lib/storyblok';
 
 export default async function Page({ params }) {
-	const { slug } = await params;
+	let { slug } = await params;
 
+	console.log(slug);
+	let language = null;
+	if(slug && slug.length > 0){
+		const availableLanguages = ['pt-br'];
+		language = availableLanguages.includes(slug[0]) ? slug[0] : undefined;
+		if (language) {
+			slug.shift();
+			if(slug.length ==0)
+				slug = null;
+		}
+	}
+	
+	console.log(slug);
 	let fullSlug = slug ? slug.join('/') : 'hero';
+	console.log(fullSlug);
 
 	let sbParams = {
 		version: 'draft',
-		cv: Date.now()
+		cv: Date.now(),
+		language
 	};
 
+	let response = null
 	const storyblokApi = getStoryblokApi();
-	let { data } = await storyblokApi.get(`cdn/stories/${fullSlug}`, sbParams);
+	try {
+		 response = await storyblokApi.get(`cdn/stories/${fullSlug}`, sbParams);	
+	} catch (error) {
+		response = await storyblokApi.get(`cdn/stories/hero`, sbParams);
+	}
+	const data = response.data;
+	console.log(data);
 
 	//useStoryblokBridge(data.story);
 
